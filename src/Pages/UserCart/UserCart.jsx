e3a2b712811e2a1f9e0f88e2f526440fd9bb9639
SabreenaGeelani <sabreenageelani@gmail.com>
2024-08-28 14:48:51 +0530

    stripe integrateddd

--- src/Pages/UserCart/UserCart.jsx
+++ src/Pages/UserCart/UserCart.jsx
@@ -143,7 +143,7 @@ const UserCart = () => {
                   <h6>$15.99</h6>
                 </span>
                 <div>
-                  <button onClick={checkoutHandler}>Checkout NOW</button>
+                  <button onClick={checkoutHandler}>Checkout</button>
                   <p>
                     <FontAwesomeIcon icon={faArrowRight} />
                   </p>
